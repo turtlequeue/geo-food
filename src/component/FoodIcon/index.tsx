1002a85b3da0686339924feaa258a2bdb4a82724
platane <me@platane.me>
2020-02-19 09:42:37 +1100

    :rocket: typescript allows image import

--- src/component/FoodIcon/index.tsx
+++ src/component/FoodIcon/index.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-// @ts-ignore
 import pizza from "../../asset/image/1f32e.png";
-// @ts-ignore
 import burrito from "../../asset/image/1f32f.png";
-// @ts-ignore
 import fries from "../../asset/image/1f355.png";
-// @ts-ignore
 import taco from "../../asset/image/1f35f.png";
 
-// @ts-ignore
 import pizza64 from "../../asset/image/1f32e-64.png";
-// @ts-ignore
 import burrito64 from "../../asset/image/1f32f-64.png";
-// @ts-ignore
 import fries64 from "../../asset/image/1f355-64.png";
-// @ts-ignore
 import taco64 from "../../asset/image/1f35f-64.png";
 
 export const food_image = {
