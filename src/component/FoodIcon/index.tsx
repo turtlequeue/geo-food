dbad87860b543062ac502c7593561c8a7c8a31af
nha <nha@users.noreply.github.com>
2020-05-01 15:33:23 +0100

    prep

--- src/component/FoodIcon/index.tsx
+++ src/component/FoodIcon/index.tsx
@@ -9,7 +9,6 @@ import burrito from "../../asset/image/1f32f.png";
 import fries from "../../asset/image/1f355.png";
 // @ts-ignore
 import taco from "../../asset/image/1f35f.png";
-
 // @ts-ignore
 import pizza64 from "../../asset/image/1f32e-64.png";
 // @ts-ignore
@@ -20,20 +19,20 @@ import fries64 from "../../asset/image/1f355-64.png";
 import taco64 from "../../asset/image/1f35f-64.png";
 
 export const food_image = {
-  taco,
-  fries,
-  pizza,
-  burrito
+    taco,
+    fries,
+    pizza,
+    burrito
 };
 export const food_image_small = {
-  taco: taco64,
-  fries: fries64,
-  pizza: pizza64,
-  burrito: burrito64
+    taco: taco64,
+    fries: fries64,
+    pizza: pizza64,
+    burrito: burrito64
 };
 
 export const FoodIcon = ({ food, ...props }) => (
-  <Img {...props} alt={`${food} icon`} src={food_image[food]} />
+    <Img {...props} alt={`${food} icon`} src={food_image[food]} />
 );
 
 const Img = styled.img`
