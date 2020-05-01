031629c6e30db955550234559e3e4d44f22f1450
nha <nha@users.noreply.github.com>
2020-05-01 21:58:57 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.40eab62fc40625cf775a16c11a4cd60d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398dc719a96a60d083bc673211dcc88a",
+    "url": "/geo-food/index.html"
+  },
+  {
+    "revision": "8806e1dfd8df5332ef4b",
+    "url": "/geo-food/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "45d729842d60a79a4a46",
+    "url": "/geo-food/static/js/2.6f9f792f.chunk.js"
+  },
+  {
+    "revision": "cca272377c0c0db24f238837185d7099",
+    "url": "/geo-food/static/js/2.6f9f792f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8806e1dfd8df5332ef4b",
+    "url": "/geo-food/static/js/main.a69cf816.chunk.js"
+  },
+  {
+    "revision": "3abbe21923a831ec7a6b",
+    "url": "/geo-food/static/js/runtime-main.2ee28902.js"
+  },
+  {
+    "revision": "b31f1c87af22de1bdf486acb8ba4d057",
+    "url": "/geo-food/static/media/1f32e-64.b31f1c87.png"
+  },
+  {
+    "revision": "e7199aab7ae3a28f34363c2d5f36df79",
+    "url": "/geo-food/static/media/1f32e.e7199aab.png"
+  },
+  {
+    "revision": "56080a62dcba14a74192ca159135ffa1",
+    "url": "/geo-food/static/media/1f32f-64.56080a62.png"
+  },
+  {
+    "revision": "8eeb3ae7060f0cf3ae8067e07106f6f4",
+    "url": "/geo-food/static/media/1f32f.8eeb3ae7.png"
+  },
+  {
+    "revision": "bfd42f297d7b8ff977226c7356f0a538",
+    "url": "/geo-food/static/media/1f355-64.bfd42f29.png"
+  },
+  {
+    "revision": "3be17955f3011a6fcecd53e8a2a57246",
+    "url": "/geo-food/static/media/1f355.3be17955.png"
+  },
+  {
+    "revision": "1832e2381d0e275421dad1f88bfe636e",
+    "url": "/geo-food/static/media/1f35f-64.1832e238.png"
+  },
+  {
+    "revision": "e342aa6527a77774280545abbe78caac",
+    "url": "/geo-food/static/media/1f35f.e342aa65.png"
+  }
+]);
